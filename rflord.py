ebd5f70a8b6791ab08c9af05a4e2abdcf4318604
ihorman <ihorman@users.noreply.github.com>
2026-07-18 14:32:05 +0300

    v0.5.26: Fix VERSION constant (was stuck at v0.5.13)
    Author: Ihor Kolodyuk

--- rflord.py
+++ rflord.py
@@ -22,7 +22,7 @@ import curses
 from spy_db import identify_spy_device, get_signal_icon, get_threat_icon, pad_icon
 
 # Config
-VERSION = "v0.5.13"
+VERSION = "v0.5.25"
 INTERVAL = 120
 TTS_VOICE = "en-US-SteffanNeural"
 VOICE_THRESHOLD = -15
